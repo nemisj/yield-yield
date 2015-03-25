00ea901eccd32218017a4f0964b06a14f921319f
Maks Nemisj <maks@nemisj.com>
2015-03-25 23:21:15 +0100

    arguments passing to the generator

--- test/index.test.js
+++ test/index.test.js
@@ -2,6 +2,8 @@ describe('index.js', function () {
 
   var y2 = require('../index.js');
   var expect = require('chai').expect;
+  var fs = require('fs');
+  var async = require('async');
 
   it('should be a function', function () {
     expect(y2).to.be.a('function');
@@ -80,7 +82,11 @@ describe('index.js', function () {
 
   it('should pass error back as first argument', function (done) {
     var fnc = y2(function *() {
+      var cb = yield;
+
       throw new Error('Testing error');
+
+      return yield;
     });
 
     fnc(function (e) {
@@ -88,6 +94,42 @@ describe('index.js', function () {
 
       return done();
     });
+  });
+
+  //
+  // Passing arguments via callback, like old node.js style
+  //
+  it('should pass arguments to the generator', function (done) {
+    var fnc = y2(function *() {
+      return yield fs.readFile(__filename, { encoding: 'utf8' }, yield);
+    });
+
+    fnc('arg1', 'arg2', function (err, contents) {
+      expect(err).to.be.not.ok;
+      expect(contents).to.include('describe(\'index.js\',');
+      return done();
+    });
+  });
+
+  it('should work with async', function (done) {
+    var fnc = y2(function *() {
+
+      var result = yield async.map(['a', 'b', 'c'], function (item, cb) {
+        setTimeout(function () {
+          cb(null, item + ':change');
+        }, 10);
+      }, yield);
+
+      return result;
+    });
+
+    fnc(function (err, result) {
+      expect(result).to.deep.equal([
+        'a:change', 'b:change', 'c:change'
+      ]);
+      done();
+    });
+
   });
 
 });
