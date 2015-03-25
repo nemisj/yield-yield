0a6cb619308e6afb24ca099b86338bfce267e2ea
Maks Nemisj <maks@nemisj.com>
2015-03-25 14:41:38 +0100

    initial commit

---
+++ test/index.test.js
@@ -0,0 +1,10 @@
+describe('index.js', function () {
+
+  var y2 = require('../index.js');
+  var expect = require('chai').expect;
+
+  it('should be a function', function () {
+    expect(y2).to.be.a('function');
+  });
+
+});
