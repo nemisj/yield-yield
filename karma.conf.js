989510f1cbc11b1a605d564634c3385e66fb64f6
Maks Nemisj <maks@nemisj.com>
2015-04-14 14:27:28 +0200

    Testing via browsers

--- karma.conf.js
+++ karma.conf.js
@@ -5,25 +5,29 @@ module.exports = function (config) {
       base: 'SauceLabs',
       browserName: 'chrome',
       platform: 'Windows 7',
-      version: '35'
+      version: '41'
     },
+
     sl_firefox: {
       base: 'SauceLabs',
       browserName: 'firefox',
       version: '37'
     },
-    sl_ios_safari: {
-      base: 'SauceLabs',
-      browserName: 'iphone',
-      platform: 'OS X 10.9',
-      version: '7.1'
-    },
-    sl_ie_11: {
-      base: 'SauceLabs',
-      browserName: 'internet explorer',
-      platform: 'Windows 8.1',
-      version: '11'
-    }
+
+//    sl_ios_safari: {
+//      base: 'SauceLabs',
+//      browserName: 'iphone',
+//      platform: 'OS X 10.9',
+//      version: '7.1'
+//    },
+
+//    sl_ie_11: {
+//      base: 'SauceLabs',
+//      browserName: 'internet explorer',
+//      platform: 'Windows 8.1',
+//      version: '11'
+//    }
+
   };
 
   config.set({
